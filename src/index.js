39351a53247d2890f852fd0a57e821b5c53f80e1
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-03-20 18:04:59 +0300

    BAK BAK BAK

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { ContextProvider } from "./Context/MainContext";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./css/main.css"
 
 ReactDOM.render(
   <React.StrictMode>
