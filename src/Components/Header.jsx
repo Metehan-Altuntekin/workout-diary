d496cb5055771343bfcecf8942b4fd2f1365824a
burakndmr <buraknasifdemir123@gmail.com>
2022-03-20 22:46:53 +0300

    make a component for home icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Account from "./Account";
-
+import Home from "../assets/icons/HomeIcon.jsx";
 const Header = ({ isOpen, setIsOpen }) => {
   console.log(isOpen);
   return (
     <header>
       <h1>Welcome Larry</h1>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <Home />
+        </Link>
         <Link to="/workout/start-workout">Workout</Link>
         <Link to="/stats">Stats</Link>
         <Link to="/settings">Settings</Link>
