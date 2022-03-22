50a5d1285d2ac98d6d4c4ee38befc4a891a49f26
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-03-22 22:27:07 +0300

    some styling on page nav

--- src/Pages/Workout.jsx
+++ src/Pages/Workout.jsx
@@ -5,9 +5,11 @@ import AddWorkout from "./Workout-Sections/AddWorkout";
 const Workout = () => {
   return (
     <>
-      <nav>
-        <Link to="start-workout">Start Workout</Link>
+      <nav className="sections">
+        <Link to="start-workout" className="active">Start Workout</Link>
         <Link to="add-workout">Add Workout</Link>
+        <Link to="#">Workout History</Link>
+        <Link to="#">Workout Templates</Link>
       </nav>
       <Routes>
         <Route index path="/start-workout" element={<StartWorkout />} />
